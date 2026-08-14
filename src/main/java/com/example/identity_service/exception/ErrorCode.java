6d2844ddca1275106c9fb4a54418fe7f75250c23
huudatt02 <huudatnguyen1912@gmail.com>
2026-08-14 16:50:45 +0700

    perf: optimize error code

--- src/main/java/com/example/identity_service/exception/ErrorCode.java
+++ src/main/java/com/example/identity_service/exception/ErrorCode.java
@@ -13,20 +13,18 @@ public enum ErrorCode {
     EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "Email already exists"),
     ROLE_NOT_FOUND(HttpStatus.NOT_FOUND, "Role not found"),
     OLD_PASSWORD_INCORRECT(HttpStatus.BAD_REQUEST, "Old password is incorrect"),
+    USER_ACCOUNT_DISABLED(HttpStatus.FORBIDDEN, "User account is disabled"),
+    EMAIL_NOT_VERIFIED(HttpStatus.FORBIDDEN, "Email not verified"),
+    FAILED_TO_SEND_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to send email"),
 
     UNAUTHENTICATED(HttpStatus.UNAUTHORIZED, "Unauthenticated"),
     ACCESS_DENIED(HttpStatus.FORBIDDEN, "Access denied"),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid token"),
     INVALID_TOKEN_TYPE(HttpStatus.UNAUTHORIZED, "Invalid token type"),
-    USER_ACCOUNT_DISABLED(HttpStatus.FORBIDDEN, "User account is disabled"),
-    EMAIL_NOT_VERIFIED(HttpStatus.FORBIDDEN, "Email not verified"),
-
     TOKEN_BLACKLISTED(HttpStatus.UNAUTHORIZED, "Token is blacklisted"),
     MISSING_JTI_CLAIM(HttpStatus.BAD_REQUEST, "Missing jti claim in token"),
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "Refresh token not found"),
-    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "Invalid request"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
-    FAILED_TO_SEND_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to send email");
+    ;
 
     private final HttpStatus status;
     private final String message;
